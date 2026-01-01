732adf1bd1615b53b43f98b343bbc6013f0b137c
madhukayal88-glitch <madhukayal88@gmail.com>
2026-01-01 06:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function removecolor() {
+    // 1. Get the select element by its ID
+    const dropdown = document.getElementById("colorSelect");
+
+    // 2. Get the index of the currently selected option
+    const selectedIndex = dropdown.selectedIndex;
+
+    // 3. Validation: Ensure an item is actually selected before trying to remove
+    if (selectedIndex !== -1) {
+        // 4. Remove the option at the selected index
+        dropdown.remove(selectedIndex);
+    }
+}
